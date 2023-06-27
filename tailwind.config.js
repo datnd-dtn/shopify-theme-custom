8cb4a837ccefef1c26826a62845168d76a45d3bb
Dat Nguyen <114894004+datnd-dtn@users.noreply.github.com>
2023-06-27 11:35:27 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './sections/*.liquid',
+    './layout/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
+    './snippets/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
